e7e66953e2eb443b914810835a59e6a77a0f6604
Александр Щепетов <cherryknight@yandex.ru>
2021-12-12 20:11:03 +0300

    Enable DEBUG

--- web/backend/settings.py
+++ web/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9ih(y(i*nl!w0jkq!vx(p=f#m@pzwx*7_8rs#6*y6e+deo#5+s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
