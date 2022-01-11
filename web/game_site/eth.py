791bffabbbd2dc6beb86cd728e20485e00a3e0f1
Александр Щепетов <cherryknight@yandex.ru>
2022-01-12 01:33:03 +0300

    Update hash of changed contract function

--- web/game_site/eth.py
+++ web/game_site/eth.py
@@ -3,7 +3,7 @@ from brownie import network, Contract
 netwok_name = "ropsten"
 game_address = "0x1A0F477F516762Aaa11a92296776c5F044a62515"
 paint_method_hash = "19ef4ef0"
-check_time_method_hash = "39277da8"
+check_time_method_hash = "999329ad"
 
 if not network.is_connected():
 	network.connect(netwok_name)
